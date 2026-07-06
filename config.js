cdf93e11531564443da3bd1ac942f100da833e2e
Shubham-Safaya <safayashubham@gmail.com>
2026-07-06 13:59:09 -0500

    Update Supabase anon key in config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 // the database (see schema.sql). Committing this file is expected.
 window.HISAAB_CONFIG = {
   SUPABASE_URL: "https://arekedbldtedprtouvme.supabase.co",
-  SUPABASE_ANON_KEY: "PASTE_SUPABASE_ANON_KEY",
+  SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFyZWtlZGJsZHRlZHBydG91dm1lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMzNTQ5MzcsImV4cCI6MjA5ODkzMDkzN30.CLHoZxp3Gp-_I-hkVRL0LIpJnfo_IFXNPJXE1awflDg",
 };
